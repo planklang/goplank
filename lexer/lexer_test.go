d5e1ccc17b5271ca83fd933ac04dc8b8fef854f2
William Hergès <anhgelus@anhgelus.world>
2025-07-22 19:06:22 +0200

    test(lexer): simple

---
+++ lexer/lexer_test.go
@@ -0,0 +1,32 @@
+package lexer
+
+import "testing"
+
+func TestLex(t *testing.T) {
+	res := Lex("axis")
+	if len(res) != 1 {
+		t.Error("Expected 1, got ", len(res))
+		printLex(t, res)
+	}
+	if res[0].Type != keywordType || res[0].Literal != "axis" {
+		t.Error("Expected keyword(axis), got ", res[0])
+	}
+	res = Lex("axis x")
+	if len(res) != 2 {
+		t.Error("Expected 2, got ", len(res))
+	}
+	if res[0].Type != keywordType || res[0].Literal != "axis" {
+		t.Error("Expected keyword(axis), got ", res[0])
+	}
+	if res[1].Type != literalType || res[1].Literal != "x" {
+		t.Error("Expected literal(x), got ", res[1])
+	}
+}
+
+func printLex(t *testing.T, lexs []*Lexer) {
+	s := ""
+	for _, l := range lexs {
+		s += l.String() + " "
+	}
+	t.Log(s[:len(s)-1])
+}
