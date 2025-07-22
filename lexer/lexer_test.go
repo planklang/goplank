ed8c3ddb75a33db0d539c5daff3b9bf67707aea9
William Hergès <anhgelus@anhgelus.world>
2025-07-22 19:07:16 +0200

    test(lexer): comments

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,6 +21,14 @@ func TestLex(t *testing.T) {
 	if res[1].Type != literalType || res[1].Literal != "x" {
 		t.Error("Expected literal(x), got ", res[1])
 	}
+	res = Lex("axis # hello world")
+	if len(res) != 1 {
+		t.Error("Expected 1, got ", len(res))
+		printLex(t, res)
+	}
+	if res[0].Type != keywordType || res[0].Literal != "axis" {
+		t.Error("Expected keyword(axis), got ", res[0])
+	}
 }
 
 func printLex(t *testing.T, lexs []*Lexer) {
