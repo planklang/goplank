17aea3046e24a3a613d8e46d8bd6cf5c8e8edca6
William Hergès <anhgelus@anhgelus.world>
2025-07-24 17:30:17 +0200

    test(parser): argument tuple len > 1

--- parser/ast_test.go
+++ parser/ast_test.go
@@ -33,4 +33,32 @@ func TestParse(t *testing.T) {
 	if len(axis.Modifiers) != 0 {
 		t.Error("Expected zero modifiers, got", len(axis.Modifiers))
 	}
+
+	lex, err = lexer.Lex("axis x 'Label'")
+	if err != nil {
+		t.Log(lex)
+		t.Fatal(err)
+	}
+	tree, err = Parse(lex)
+	if err != nil {
+		t.Log(tree)
+		t.Fatal(err)
+	}
+	if len(tree.Body) != 1 {
+		t.Errorf("Excepted 1, got %d", len(tree.Body))
+	}
+	axis, ok = tree.Body[0].(*Axis)
+	if !ok {
+		t.Error("Expected Axis, got", tree.Body[0])
+	}
+	if axis.Target != "x" {
+		t.Error("Expected x, got", axis.Target)
+	}
+	if axis.Label != "Label" {
+		t.Error("Expected Label, got", axis.Label)
+	}
+	//TODO: check range
+	if len(axis.Modifiers) != 0 {
+		t.Error("Expected zero modifiers, got", len(axis.Modifiers))
+	}
 }
