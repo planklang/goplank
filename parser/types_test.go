f3b29e749c1bdc2c3138c648ceeecc430ff89f2f
William Hergès <anhgelus@anhgelus.world>
2025-07-24 15:48:25 +0200

    test(parser): types' is

---
+++ parser/types_test.go
@@ -0,0 +1,31 @@
+package parser
+
+import "testing"
+
+func TestLiteralType_Is(t *testing.T) {
+	generalTest(t, []Type{StringType, IntType, FloatType, DefaultLiteralType})
+}
+
+func TestTupleType_Is(t *testing.T) {
+	generalTest(t, []Type{NewTupleType(StringType), NewTupleType(IntType), NewTupleType(StringType, IntType)})
+}
+
+func TestListType_Is(t *testing.T) {
+	generalTest(t, []Type{NewListType(StringType), NewListType(IntType), NewListType(FloatType)})
+}
+
+func generalTest(t *testing.T, typs []Type) {
+	for i, typ := range typs {
+		for j, t2 := range typs {
+			if i == j {
+				if !typ.Is(t2) {
+					t.Errorf("%s is not a type %s", typ, t2)
+				}
+			} else {
+				if typ.Is(t2) {
+					t.Errorf("%s is not a type %s", typ, t2)
+				}
+			}
+		}
+	}
+}
